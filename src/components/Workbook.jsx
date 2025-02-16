33554703033d05e3a40afe9868c4fceeb3822049
Kah-ul Kim <kahul.kim@gmail.com>
2025-02-16 17:48:45 +0900

    Add num key button

--- src/components/Workbook.jsx
+++ src/components/Workbook.jsx
@@ -24,6 +24,19 @@ const Workbook = ({ problem, input, setInput, handleSubmit }) => {
                     제출
                 </button>
             </div>
+            <div className="num-pad">
+                <button>0</button>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>4</button>
+                <br />
+                <button>5</button>
+                <button>6</button>
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+            </div>
         </div>
     );
 };
